6fd6cf035dbd7265426e28ed15fb5b6e1b934c7c
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-03-30 20:11:40 +0000

    update links
    git-svn-id: https://code.astraw.com/motmot/trunk/pygxinput@569 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -7,8 +7,15 @@ setup(name='pygxinput',
 include 3Dconnexion Space Navigator and Wacom tablets.
 
 For information about how to setup the Space Navigator as an XInput
-device, see http://www.fedorawiki.de/index.php/SpaceNavigator (in
-German).
+device, see:
+
+ * http://www.fedorawiki.de/index.php/SpaceNavigator (in German)
+ * http://www.3dconnexion.com/forum/viewtopic.php?p=1431&sid=9cb59f09ea21510071372daf670e1f8b#1431
+ * http://www.3dconnexion.com/forum/viewtopic.php?p=7683#7683
+
+For an alternative Space Navigator driver (currently not XInput compatible), see:
+
+ * http://spacenav.sourceforge.net/
 
 """,
       version='0.0.1', # keep in sync with pygxinput/pygxinput.py
